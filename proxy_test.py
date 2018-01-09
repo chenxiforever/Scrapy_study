a7db624a68167148d8b9b8a999d957b63f9b9f7a
chenxiforever <71562701@qq.com>
2018-01-09 23:28:17 +0800

    获取代理服务器IP地址函数，需要继续完善IP验证，生成代理IP列表写入数据库。

--- proxy_test.py
+++ proxy_test.py
@@ -4,6 +4,8 @@
 from urllib.request import urlopen
 import requests
 from bs4 import BeautifulSoup
+import socket
+
 
 header = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0'
@@ -23,7 +25,21 @@ def get_IPList():
             IPList.append(result)
     return IPList
 
-IPList = get_IPList()
-for item in IPList:
-    print(item)
+def validateIp(proxy):
+    url = "http://ip.chinaz.com/getip.aspx"
+    f = open("E:\ip.txt", "w")
+    socket.setdefaulttimeout(3)
+    for i in range(0, len(proxy)):
+        try:
+            ip = proxy[i].strip().split("\t")
+            proxy_host = "http://" + ip[0] + ":" + ip[1]
+            proxy_temp = {"http": proxy_host}
+            res = urllib.urlopen(url, proxies=proxy_temp).read()
+            f.write(proxy[i] + '\n')
+            print
+            proxy[i]
+        except Exception as e:
+            continue
+    f.close()
+
 
