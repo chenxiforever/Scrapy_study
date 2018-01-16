0072d751f307ce7fef0d397aa26a6634f813ef84
chenxiforever <71562701@qq.com>
2018-01-16 22:33:53 +0800

    提取酒店摘要数据并加入数据库，存在问题：未能过验证码、不包含获取每个酒店详细评论。

--- xiecheng.py
+++ xiecheng.py
@@ -5,8 +5,12 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import re
+import MySQLdb
+from warnings import filterwarnings
+filterwarnings('ignore', category = MySQLdb.Warning)            #过滤数据库警告
 
-
+conn = MySQLdb.connect(host='127.0.0.1', user='root', passwd='chenxi1983##', port=3306, charset='utf8')
+cursor = conn.cursor()
 BaseUrl = 'http://hotels.ctrip.com'
 url = 'http://hotels.ctrip.com/domestic-city-hotel.html'
 jsonUrl = 'http://hotels.ctrip.com/Domestic/Tool/AjaxHotelList.aspx'
@@ -21,7 +25,12 @@ headers = {
 }
 
 def getCityId():
-    html = requests.get(url).text
+    head = {
+        'Cookie':'Cookie:AHeadUserInfo=VipGrade=0&UserName=&NoReadMessageCount=0&U=F0B6A150C5D71C369827855DA7DDAD85; _abtest_userid=43139b9b-aa71-4ae6-a075-16f688b5c913; __guid=208231307.3690029046937470000.1510971618992.3616; HotelCityID=95split%E4%B8%8A%E6%B5%B7splitEmeishansplit2018-01-11split2018-01-12split0; Union=SID=155952&AllianceID=4897&OUID=baidu81|index|||; Session=SmartLinkCode=U155952&SmartLinkKeyWord=&SmartLinkQuary=&SmartLinkHost=&SmartLinkLanguage=zh; adscityen=Neijiang; traceExt=campaign=CHNbaidu81&adid=index; appFloatCnt=4; manualclose=1; HotelDomesticVisitedHotels1=4374378=0,0,4.6,403,/200g0g00000086ena9ACA.jpg,&2298680=0,0,4.6,2803,/200p0k000000b1zndCF28.jpg,&916432=0,0,4.7,281,/200k0j000000afyk849B1.jpg,; ASP.NET_SessionId=rvh2iiyqx1iljypelbnnourg; OID_ForOnlineHotel=15109708813313wom8k1516104541638102002; _bfa=1.1510970881331.3wom8k.1.1516104534939.1516107596975.7.45; _bfs=1.14; _RF1=110.190.174.223; _RSG=5EaCReE6XAB.Swux0Yip78; _RDG=28dde1ad3feff729a0118f529722046919; _RGUID=b2a118ea-1b32-4385-81f3-b2203be3a73e; Mkt_UnionRecord=%5B%7B%22aid%22%3A%224897%22%2C%22timestamp%22%3A1516108607548%7D%5D; _ga=GA1.2.806793281.1510971657; _gid=GA1.2.1985413515.1516026245; __zpspc=9.5.1516107599.1516108607.5%231%7Cbaidu%7Ccpc%7Cbaidu81%7C%25E6%2590%25BA%25E7%25A8%258B%7C%23; _jzqco=%7C%7C%7C%7C1516026244725%7C1.227931446.1510971656550.1516108587332.1516108607605.1516108587332.1516108607605.undefined.0.0.25.25; _bfi=p1%3D102086%26p2%3D102086%26v1%3D45%26v2%3D43; MKT_Pagesource=PC',
+        'Referer':'http://hotels.ctrip.com/',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    }
+    html = requests.get(url,head).text
     bsObj = BeautifulSoup(html,'lxml')
     hotelList = bsObj.findAll('dl',{'class':'pinyin_filter_detail layoutfix'})
     areaList = hotelList[0].findAll('a')
@@ -32,34 +41,40 @@ def getCityId():
 
 def getPageNum(cityId):
     FormData = {
-        'cityId': cityId,
-        'page': 1,
+        'cityId':cityId,
+        'page':1,
     }
-    html = requests.post(url, FormData, headers).text
+    html = requests.post(jsonUrl,FormData,headers).text
     dataJson = json.loads(html)
-    hotelCount = dataJson['totalMsg'].split('>')[1].split('<')[0]  # hotelCount为酒店数量
-    pageSize = divmod(int(hotelCount), 25)
+    hotelCount = dataJson['hotelAmount']                            #hotelCount为酒店数量
+    pageSize = divmod(int(hotelCount),25)
     if pageSize[1]:
         pageNum = pageSize[0] + 1
     else:
-        pageNum = pageSize
-    return pageNum
+        pageNum = pageSize[0]
+    return pageNum                                                  #pageNum为列表页数
+
 
 def getSummaryInfo(pageNum,cityId):
-    for page in range(1, pageNum + 1):
+    cursor.execute('Create database if not EXISTS hotelInfo')
+    cursor.execute('use hotelInfo')
+    cursor.execute(
+        'Create table if NOT EXISTS hotelSummary(id int primary key not null,name char(255) not null,score char(10) not null,dpcount char(10) not null,dpscore char(10) not null,stardesc char(255) not null,address char(255) not null,url char(255) not null);')
+    conn.commit()
+    for page in range(1, pageNum+1):
         FormData = {
             'cityId': cityId,
             'page': page,
         }
         print('第%d页' % page)
         try:
-            html = requests.post(url, FormData, headers).text
-            print(html)
+            html = requests.post(jsonUrl, FormData, headers).text
             dataJson = json.loads(html)
             hotelList = dataJson['hotelPositionJSON']
             for hotelInfo in hotelList:
-                hotelSummaryList.append(hotelInfo['name'] + '|' + hotelInfo['score'] + '|' + hotelInfo['dpcount'] + '|' + hotelInfo['dpscore'] + '|' + hotelInfo['stardesc'] + '|' + hotelInfo['address'] + '|' + hotelInfo['id'] + '|' + BaseUrl + hotelInfo['url'])
-                print(hotelInfo)
+                url = BaseUrl + hotelInfo['url']
+                cursor.execute(
+                    "insert into hotelSummary(id,name,score,dpcount,dpscore,stardesc,address,url) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s')" % (hotelInfo['id'], hotelInfo['name'], hotelInfo['score'], hotelInfo['dpcount'],hotelInfo['dpscore'], hotelInfo['stardesc'], hotelInfo['address'], url))
                 print('酒店名称:', hotelInfo['name'])  # 酒店名称
                 print('酒店评分:', hotelInfo['score'])  # 酒店评分
                 print('点评数:', hotelInfo['dpcount'])  # 点评数
@@ -68,17 +83,25 @@ def getSummaryInfo(pageNum,cityId):
                 print('地址:', hotelInfo['address'])  # 地址
                 print('酒店id:', hotelInfo['id'])  # 酒店id
                 print('详细页地址:', BaseUrl + hotelInfo['url'])  # 详细页地址
+                print('插入一条数据成功!')
+                conn.commit()
                 print('\n')
         except Exception as e:
             print(e)
             continue
-    return hotelSummaryList
 
-idList = getCityId()
-for item in idList:
-    pageNum = getPageNum(item.split('|')[1])
-    print(pageNum)
-
-# getSummaryInfo()
-
-
+def main():
+    idList = getCityId()
+    for item in idList:
+        print(item)
+        id = item.split('|')[1]
+        try:
+            pageNum = getPageNum(id)
+            # print(id,pageNum)
+            getSummaryInfo(pageNum, id)
+        except Exception as e:
+            continue
+    cursor.close()
+    conn.close()
+if __name__ =='__main__':
+    main()
